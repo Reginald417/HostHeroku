c127458cd2a1f20343ed1f35958958dfd0491b16
reginald417 <reginaldlyr37.19@ichat.sp.edu.sg>
2020-12-10 22:23:18 +0800

    hihihihi

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var {app}=require('./app.js');
 // var port=3000;
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 
 var server=app.listen(port,function(){
     console.log("App hosted at localhost:"+port);
